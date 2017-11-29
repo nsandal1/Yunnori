d97f3ef05fa7f97f6c087ef071642e9b275d3e41
lhiexs <lhiexs@gmail.com>
2017-11-28 22:16:14 -0200

    Update Player.py
    Now with Director Commentary

--- Player.py
+++ Player.py
@@ -6,9 +6,11 @@ class Player():
     def __init__(self,name="olivia", color="Purple",order=1):
         self.name=name
         self.color=color
-        self.pieces=(Piece(),Piece(),Piece(),Piece())
-        self.order=order
+        self.pieces=(Piece(),Piece(),Piece(),Piece())   #defines the pieces state
+        self.order=order                                #defines the player order state
 
+        
+    #bunch of functions that return states
     def retPieceN(self,n):
         return self.pieces[n]
 
@@ -27,16 +29,17 @@ foo=input("starting...")
 
 class Game():
 
-    def __init__(self, player1=Player(), player2=Player("luis","green",2)):
+    def __init__(self, player1=Player(), player2=Player("luis","green",2)): #initializes two players
         self.player1=player1
         self.player2=player2
         self.players=(self.player1,self.player2)
 
+    #will expand this to return more basic players information    
     def retPlayers(self):
-        print(self.players)
+        print(self.players)  #just an object handle for the moment
 
     def retPlayerN(self,n):
-        return self.players[n-1]
+        return self.players[n-1]    #returns player object for player n
 
 
 game1=Game()
