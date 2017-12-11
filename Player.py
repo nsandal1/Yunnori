4fdd08abd415f5537cb78709f7533f99836cbd3c
lhiexs <lhiexs@gmail.com>
2017-12-11 01:17:11 -0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -5,12 +5,11 @@ class Player():
     def __init__(self,name="olivia", color="purple",order=1):
         self.name=name
         self.color=color
-        self.pieces=(Piece.Piece(name,1),Piece.Piece(name,2),Piece.Piece(name,3),Piece.Piece(name,4))   #piece should contain player name
+        self.pieces=(Piece.Piece(name,1,color),Piece.Piece(name,2,color),Piece.Piece(name,3,color),Piece.Piece(name,4,color))   #piece should contain player name
         self.order=order
-        self.promoted=0
         self.win = 0
 
-    def retPieceN(self,n):
+    def retPieceN(self,n):          #returns the Nth piece
         return self.pieces[n-1]
 
     def retOrder(self):
@@ -21,10 +20,8 @@ class Player():
 
     def retName(self):
         return self.name
+    
+    def retWin(self):
+        return self.win
 
-    def retPromoted(self):
-        return self.promoted
-
-    def incPromoted(self,n=1):
-        self.promoted+=n
     
